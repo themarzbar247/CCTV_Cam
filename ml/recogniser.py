a365b93312367d35f5a196a5a88860210440b39d
Myles P <myles.puddephatt@bt.com>
2021-05-31 19:55:38 +0100

    save alerts in alert folder and save new images in input dir, this completes the loop

--- ml/recogniser.py
+++ ml/recogniser.py
@@ -2,37 +2,43 @@ import cv2
 from time import time
 import sys
 import threading
-from subprocess_wrapper import recieve,send
+from subprocess_wrapper import recieve, send
 from commands import Recognise, Retrain, Quit, Decide
+import json
 
 def main():
     retrainer = Retrainer()
     running = True
     while running:
         command = recieve()
-        if isinstance(command,Quit):
+        if isinstance(command, Quit):
             running = False
-        elif isinstance(command,Recognise):
-            face_id, confidence = retrainer.recognizer.predict(command.face) 
-            command = Decide.create_from_get_frame(command, face_id, confidence)
-        elif isinstance(command,Retrain):
-            retrainer.run(command.trainer_path)
+        elif isinstance(command, Recognise):
+            face_id, confidence = retrainer.recognizer.predict(command.face)
+            name = retrainer.names[face_id]
+            command = Decide.create_from_get_frame(command, face_id, name, confidence)
+        elif isinstance(command, Retrain):
+            retrainer.run(command.trainer_path, command.name_path)
             while command.wait and retrainer.recognizer is None:
                 pass
         send(command)
 
+
 class Retrainer:
     recognizer = None
-    
-    def run(self, trainer_path=None):
-        if trainer_path is not None:
-            self.trainer_path = trainer_path
-            thread = threading.Thread(target=self.retrain, daemon=True)
-            thread.start()
+
+    def run(self, trainer_path, name_path):
+        self.name_path = name_path
+        self.trainer_path = trainer_path
+        thread = threading.Thread(target=self.retrain, daemon=True)
+        thread.start()
 
     def retrain(self):
+        with open(self.name_path, "r") as f:
+            self.names = json.load(f)
         recognizer = cv2.face.LBPHFaceRecognizer_create()
         self.recognizer = recognizer.read(self.trainer_path)
+
 
 if __name__ == "__main__":
     main(*sys.argv[1:])
